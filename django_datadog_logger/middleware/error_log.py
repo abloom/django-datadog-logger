3f307c64e6c3dd370106a4a6cd895d8d29078c58
Andrew Bloom <andrew@turquoise.health>
2023-01-19 14:05:29 -0600

    log 404s as a warning

--- django_datadog_logger/middleware/error_log.py
+++ django_datadog_logger/middleware/error_log.py
@@ -1,4 +1,6 @@
+import traceback
 import logging
+from django.http.response import Http404
 
 
 logger = logging.getLogger(__name__)
@@ -12,7 +14,15 @@ class ErrorLoggingMiddleware:
         return self.get_response(request)
 
     def process_exception(self, request, exception):
-        logger.exception(exception)
+        if isinstance(exception, Http404):
+            log_entry_dict = {
+                "error.kind": 404,
+                "error.message": str(exception),
+                "error.stack": traceback.format_exception(exception),
+            }
+            logger.warning(f"Http404: {exception}", extra=log_entry_dict)
+        else:
+            logger.exception(exception)
 
     def process_response(self, request, response):
         return response
